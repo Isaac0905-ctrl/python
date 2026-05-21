a734feb0e7cd0f39c1a8c5b99af11ffe54107d19
Adrian Isaac Fuentes Florez <isaacfuentesflorez@gmail.com>
2026-05-21 14:50:13 +0100

    Ejercicio de listas fix-date

--- listas/fix-date/main.py
+++ listas/fix-date/main.py
@@ -1,9 +1,16 @@
 def run(input_date: str, base_year: int) -> str:
     input_date = input_date.split('/')
-    day = input_date[1]
-    month = input_date[0]
+    day = int(input_date[1])
+    month = int(input_date[0])
     year = base_year + int(input_date[2])
-    output_date = f'{day}-{month}-{year}'
+    if day < 10:
+        output_date = f'0{day}-{month}-{year}'
+    elif month < 10:
+        output_date = f'{day}-0{month}-{year}'
+    elif year < 1000:
+        output_date = f'{day}-{month}-0{year}'
+    else:
+        output_date = f'{day}-{month}-{year}'
     return output_date
 
 
